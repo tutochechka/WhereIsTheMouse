01209d6378dc822e44680058073698f9d1961de8
Sveta <tutochechka@gmail.com>
2016-05-26 02:14:46 +0300

    Working app

---
+++ WhereIsTheMouse/app/src/main/java/com/gazina/sveta/whereisthemouse/CellControl.java
@@ -0,0 +1,99 @@
+package com.gazina.sveta.whereisthemouse;
+
+/**
+ * Created by Sveta on 5/23/2016.
+ */
+public class CellControl {
+    public int getSize() {
+        return size;
+    }
+
+    private int size = 5;
+    private int gameField [][] = new int[size][size];
+
+    public int[][] getGameField() {
+        return gameField;
+    }
+    public int getRow(int position){
+        return position/size;
+    }
+    public int getColumn(int position){
+        return position%size;
+    }
+
+    public void setNewGame(){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                gameField[i][j] = 0;
+            }
+        }
+        gameField[2][2] = 1;
+
+    }
+    public int getCellType(int row, int column){
+
+        if(gameField[row][column] == 1){
+            return R.drawable.mousefound;
+        }
+
+        for (int i = 0; i < row; i++) {
+            if (gameField[i][column] == 1){
+                return R.drawable.n;
+            }
+        }
+        for (int i = row + 1; i < size; i++) {
+            if (gameField[i][column] == 1){
+                return R.drawable.s;
+            }
+        }
+        for (int i = 0; i < column; i++) {
+            if (gameField[row][i] == 1){
+                return R.drawable.w;
+            }
+        }
+        for (int i = column + 1; i < size; i++) {
+            if (gameField[row][i] == 1){
+                return R.drawable.e;
+            }
+        }
+        int j;
+
+        j = column;
+        for (int i = row -1; i >= 0 ; i--) {
+            j--;
+            if (j >= 0 && gameField[i][j] == 1) {
+                return R.drawable.nw;
+            }
+
+        }
+        j = column;
+        for (int i = row + 1; i < size ; i++) {
+            j--;
+            if (j >= 0 && gameField[i][j] == 1) {
+                return R.drawable.sw;
+            }
+
+        }
+
+        j = column;
+        for (int i = row -1; i >= 0 ; i--) {
+            j++;
+            if (j < size && gameField[i][j] == 1) {
+                return R.drawable.ne;
+            }
+
+        }
+
+        j = column;
+        for (int i = row + 1; i < size ; i++) {
+            j++;
+            if (j < size && gameField[i][j] == 1) {
+                return R.drawable.se;
+
+            }
+
+        }
+        return R.drawable.empty;
+    }
+
+}
--- WhereIsTheMouse/app/src/main/java/com/gazina/sveta/whereisthemouse/GameActivity.java
+++ WhereIsTheMouse/app/src/main/java/com/gazina/sveta/whereisthemouse/GameActivity.java
@@ -2,12 +2,34 @@ package com.gazina.sveta.whereisthemouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v, int position,
+                                    long id) {
+                CellControl control = new CellControl();
+                control.setNewGame();
+                ImageView imageView = (ImageView) v;
+                imageView.setImageResource(control.getCellType(control.getRow(position), control.getColumn(position)));
+
+
+            }
+        });
     }
 }
